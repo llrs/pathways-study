3cfadbf0dd14ec2a0feae4407575d3d7e1b20bdc
Lluís Revilla <lluis.revilla@gmail.com>
2018-05-13 22:57:36 +0200

    Use the latest features of GSEAdv

--- load.R
+++ load.R
@@ -1,94 +1,79 @@
-library("GSEABase")
+library("GSEAdv")
+library("tidyverse")
 
 # Load the files
 gmt <- list.files(path = "data/", full.names = TRUE, pattern = "entrez.gmt")
-db <- sapply(gmt, function(x){geneIds(getGmt(x))})
+db <- sapply(gmt, function(x){getGmt(x)})
+
 names(db) <- gsub("data//(.*)\\.v6\\.1\\.entrez\\.gmt",
                          replacement =  "\\1", names(db))
-databases <- sapply(gmt, function(x){
-  paths2Genes <- geneIds(getGmt(x))
 
-  genes <- unlist(paths2Genes, use.names = FALSE)
-  pathways <- rep(names(paths2Genes), lengths(paths2Genes))
-  split(pathways, genes)
-})
-names(databases) <- gsub("data//(.*)\\.v6\\.1\\.entrez\\.gmt",
-                         replacement =  "\\1", names(databases))
+gpp <- lapply(db, genesPerPathway)
+ppg <- lapply(db, pathwaysPerGene)
 
-genesPerPathway <- sapply(db, lengths)
-
-all <- sapply(names(databases), function(y){
-  sapply(databases[[y]], function(x){
-    genesPerPathway[[y]][x]
-  })
-})
-
-pathwaysPerGene <- sapply(databases, lengths)
-
-x <- "c2.cp.reactome"
-plot(table(pathwaysPerGene[[x]]))
-plot(table(genesPerPathway[[x]]))
-
-
-
-# plot(density(genesPerPathway[["c2.cp"]]), col = "red")
-lines(density(genesPerPathway[["c2.cp.reactome"]]), col = "green", xlab = "Pathways",
-      main = "Comparison of the size of genes per Pathways for multiple databases")
-# lines(density(genesPerPathway[["c2.all"]]))
-lines(density(genesPerPathway[["c2.cp.kegg"]]), col = "blue")
-lines(density(genesPerPathway[["c2.cp.biocarta"]]), col = "pink") # Out of charts
-# lines(density(genesPerPathway[["c2.cgp"]]), col = "brown")
-maxs <- sapply(genesPerPathway[c("c2.cp.biocarta", "c2.cp.reactome", "c2.cp.biocarta")],
-       function(x){max(table(x))})
-abline(v = maxs, col = c("pink", "green", "blue")[order(maxs)])
-range(maxs) # The most expected site of the pathway regardless the database
-plot(function(x) dt(x, df = 25, ncp = 10), -3, 800,
-     main = "Non-central t - Density", yaxs = "i")
-
-
-lines(density(pathwaysPerGene[["c2.cp.reactome"]]), col = "green",  xlab = "Genes",
-     main = "Comparison of the number of Pathways per gene for multiple databases")
-lines(density(pathwaysPerGene[["c2.cp.kegg"]]), col = "blue") # Out of charts
-# lines(density(pathwaysPerGene[["c2.cp"]]), col = "red")
-# lines(density(pathwaysPerGene[["c2.all"]]))
-lines(density(pathwaysPerGene[["c2.cp.biocarta"]]), col = "pink") # Out of charts
-# lines(density(pathwaysPerGene[["c2.cgp"]]), col = "brown")
-maxs <- sapply(pathwaysPerGene[c("c2.cp.biocarta", "c2.cp.reactome", "c2.cp.biocarta")],
-               function(x){a <- table(x)
-                 as.numeric(names(a)[which.max(a)])})
-abline(v = maxs, col = c("pink", "green", "blue")[order(maxs)])
-range(maxs) # If a gene has a pathway the most common are between 1 and 3
-# It looks like a t student distribution
-plot(function(x) dt(x, df = 3, ncp = 4), -3, 100,
-     main = "Non-central t - Density", yaxs = "i")
-
-# Total ~pathways
-(pathways <- length(unique(unlist(sapply(databases, function(x){
-  names(x)
-})))))
-
-# Total genes
-(genes <- length(unique(unlist(sapply(databases, function(x){
-  unlist(x)
-})))))
-
-# Analyse the pathways
-library("BioCor")
-
-for (f in seq_along(databases)){
-  o <- mpathSim(unique(unlist(databases[[f]])),
-                info = databases[[f]], method = NULL)
-  write.csv(o, file =  paste0(names(databases[f]), ".csv"))
+l2df <- function(x){
+  l <- unlist(x, use.names = FALSE)
+  data.frame(x = l,
+                   db = rep(names(x), lengths(x)),
+                   pathway = unlist(sapply(x, names), use.names = FALSE))
 }
+df_gpp <- l2df(gpp)
+df_ppg <- l2df(pp)
+colnames(df_ppg) <- c("ppg", "db", "gene")
 
-paths <- list.files(pattern = "*.csv")
-pathwaySim <- lapply(paths, read.csv)
+ggplot(df[df$db == "c1.all", ]) +
+  geom_density(aes(seq(1, max(df[df$db == "c1.all", "gpp"])), gpp, color = db)) +
+  theme_bw()
 
-gse <- read.csv("c7.all.csv", row.names = 1)
-gse <- as.matrix(gse)
-library("Hmisc")
-Ecdf(gse[upper.tri(gse)])
+gpp_t <- lapply(gpp, function(x){
+  table(x)
+})
 
-combined <- combineSources(databases)
 
-o <- mpathSim(unique(unlist(combined)), info = combined, method = NULL)
+t_num <- as.numeric(unique(unlist(sapply(gpp_t, names), use.names = FALSE)))
+t_num <- t_num[order(t_num)]
+m <- matrix(NA, nrow = max(t_num), ncol = length(gpp),
+            dimnames = list(seq_len(max(t_num)), names(gpp)))
+
+for (db in names(gpp_t)){
+  db_m <- as.matrix(gpp_t[[db]])
+  m[rownames(db_m), db] <- db_m
+}
+m <- as.data.frame(m)
+m$size <- as.numeric(rownames(m))
+ggplot(m) +
+  geom_point(aes(size, c1.all), col = "blue") +
+  geom_point(aes(size, c2.all), col = "red") +
+  geom_point(aes(size, c3.all), col = "pink") +
+  geom_point(aes(size, c4.all), col = "green") +
+  geom_point(aes(size, c6.all), col = "grey") +
+  # geom_point(aes(size, c7.all), col = "lightblue") +
+  geom_point(aes(size, h.all), col = "orange") +
+  xlab("Size of pathways") +
+  ylab("Number of pathways") +
+  theme_bw()
+
+ggplot(m) +
+  # geom_point(aes(size, c2.all), col = "blue") +
+  geom_point(aes(size, c2.cp.biocarta), col = "red") +
+  geom_point(aes(size, c2.cp.kegg), col = "pink") +
+  geom_point(aes(size, c2.cp.reactome), col = "green") +
+  geom_point(aes(size, c2.cp), col = "grey") +
+  scale_x_continuous(limits = c(0, 1150)) +
+  xlab("Size of pathways") +
+  ylab("Number of pathways") +
+  theme_bw()
+
+paths <- as.data.frame(lengths(gpp))
+colnames(paths) <- "Pathways"
+genes <- as.data.frame(lengths(ppg))
+colnames(genes) <- "Genes"
+db2 <- cbind(paths, genes)
+db2$DB <- rownames(db2)
+
+ggplot(db2) +
+  geom_bar(aes(DB, Pathways), stat = "identity") +
+  theme_classic()
+
+gpp_m <- sapply(gpp, '[', seq(max(lengths(gpp))))
+gpp_m
